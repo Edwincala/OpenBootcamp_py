05f933fa5105c1c7145cb3cd1661d82442801b82
Edwincala <edwicalacardona@gmail.com>
2023-03-02 22:05:07 -0500

    corrected the init function

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,27 +1,23 @@
 class Vehiculo:
 
-    def __init__(self):
-        color = self.color
-        ruedas = self.ruedas
-        puertas = self.puertas
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
 
 
 class Coche(Vehiculo):
-    velocidad = 0
-    cilindrada = 0
 
-    def __init__(self):
-        Velocidad = self.velocidad
-        Cilindrada = self.cilindrada
+    def __init__(self, velocidad, cilindrada):
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
 
 
-N370z = Coche()
+N370z = Coche(250, 3.7)
 N370z.color = "Platino"
 N370z.ruedas = 4
 N370z.puertas = 3
-N370z.Velocidad = 250
-N370z.Cilindrada = 3.7
 
 
 print(
-    f"El Nissan 370z viene en color {N370z.color}, tiene {N370z.ruedas} ruedas y {N370z.puertas} puertas. Alcanza una velocidad máxima de {N370z.Velocidad} km/h con una cilindrada de {N370z.Cilindrada} CC")
+    f"El Nissan 370z viene en color {N370z.color}, tiene {N370z.ruedas} ruedas y {N370z.puertas} puertas. Alcanza una velocidad máxima de {N370z.velocidad} km/h con una cilindrada de {N370z.cilindrada} CC")
