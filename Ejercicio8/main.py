feb9e19dbd09d068c5158429b93b30a759492150
Edwincala <edwicalacardona@gmail.com>
2023-03-03 19:41:49 -0500

    Ejercicio 8 módulos de aritmética

---
+++ Ejercicio8/main.py
@@ -0,0 +1,7 @@
+import operaciones as op
+
+print("Suma:", op.suma(3, 5))
+print("Resta:", op.resta(4, 3))
+print("Multiplicación:", op.mult(3, 6))
+print("División:", op.division(10, 2))
+print("División entre 0:", op.division(10, 0))
---
+++ Ejercicio8/operaciones.py
@@ -0,0 +1,14 @@
+def suma(a, b):
+    return a + b
+
+
+def resta(a, b):
+    return a - b
+
+
+def mult(a, b):
+    return a * b
+
+
+def division(a, b):
+    return a / b if b != 0 else "undefined"
