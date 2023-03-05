25ab3958e35efadb242d130ca316870c00efbba4
Edwincala <edwicalacardona@gmail.com>
2023-03-04 19:47:06 -0500

    Ejercicio 10 con archivo de salida

---
+++ Ejercicio10/ejercicio10.py
@@ -0,0 +1,8 @@
+with open('./archivo.txt', 'w') as f:
+    texto = 'Info escrita al archivo por primera vez'
+    f.write(texto)
+
+
+with open('archivo.txt', 'a') as f:
+    nuevo_texto = "\nInfo que puse después"
+    f.write(nuevo_texto)
